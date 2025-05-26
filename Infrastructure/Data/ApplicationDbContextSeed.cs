4420acd6c43dbe3e7358028478db21a566956616
Martin Rosendahl <92294097+MartyBobo@users.noreply.github.com>
2025-05-26 19:39:06 +0200

    added second resturant

--- Infrastructure/Data/ApplicationDbContextSeed.cs
+++ Infrastructure/Data/ApplicationDbContextSeed.cs
@@ -23,9 +23,7 @@ public class DataSeeder
 
             context.Users.Add(defaultUser);
             await context.SaveChangesAsync();
-        }
-
-        // Only seed if no restaurants exist
+        }        // Only seed if no restaurants exist
         if (!context.Resturants.Any())
         {
             var restaurant = new Resturant
@@ -40,7 +38,8 @@ public class DataSeeder
             context.Resturants.Add(restaurant);
             await context.SaveChangesAsync();
             
-            // Add opening hours
+            // Add opening hours for Aalborg Restaurant
+            // ...existing code...
             // Add opening hours for Monday
             context.OpeningHours.Add(new OpeningHours 
             { 
@@ -336,13 +335,333 @@ public class DataSeeder
             {
                 ResturantId = restaurant.Id,
                 ImageUrl = "/images/gallery/two.jpg"
-            });
-
-            context.GalleryImages.Add(new GalleryImage
+            });            context.GalleryImages.Add(new GalleryImage
             {
                 ResturantId = restaurant.Id,
                 ImageUrl = "/images/gallery/three.jpg"
             });
+            
+            await context.SaveChangesAsync();
+        }
+
+        // Seed Copenhagen Bistro if it doesn't exist
+        if (!context.Resturants.Any(r => r.Name == "Copenhagen Bistro"))
+        {
+            // Create second restaurant - Copenhagen Bistro
+            var restaurant2 = new Resturant
+            {
+                Name = "Copenhagen Bistro",
+                Address = "Strøget 10, 1000 København",
+                Phone = "+45 22 33 44 55",
+                Email = "copenhagen@flexybox.com",
+                IsOpen = true
+            };
+            
+            context.Resturants.Add(restaurant2);
+            await context.SaveChangesAsync();
+            
+            // Add opening hours for Copenhagen Bistro - Restaurant mode
+            context.OpeningHours.Add(new OpeningHours 
+            { 
+                ResturantId = restaurant2.Id,
+                Mode = "Restaurant",
+                Day = "Monday", 
+                StartTime = new TimeSpan(8, 0, 0),
+                EndTime = new TimeSpan(20, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours 
+            { 
+                ResturantId = restaurant2.Id,
+                Mode = "Restaurant",
+                Day = "Tuesday", 
+                StartTime = new TimeSpan(8, 0, 0),
+                EndTime = new TimeSpan(20, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours 
+            { 
+                ResturantId = restaurant2.Id,
+                Mode = "Restaurant",
+                Day = "Wednesday", 
+                StartTime = new TimeSpan(8, 0, 0),
+                EndTime = new TimeSpan(20, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours 
+            { 
+                ResturantId = restaurant2.Id,
+                Mode = "Restaurant",
+                Day = "Thursday", 
+                StartTime = new TimeSpan(8, 0, 0),
+                EndTime = new TimeSpan(20, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours 
+            { 
+                ResturantId = restaurant2.Id,
+                Mode = "Restaurant",
+                Day = "Friday", 
+                StartTime = new TimeSpan(8, 0, 0),
+                EndTime = new TimeSpan(21, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours 
+            { 
+                ResturantId = restaurant2.Id,
+                Mode = "Restaurant",
+                Day = "Saturday", 
+                StartTime = new TimeSpan(9, 0, 0),
+                EndTime = new TimeSpan(21, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours 
+            { 
+                ResturantId = restaurant2.Id,
+                Mode = "Restaurant",
+                Day = "Sunday", 
+                StartTime = new TimeSpan(10, 0, 0),
+                EndTime = new TimeSpan(18, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours 
+            { 
+                ResturantId = restaurant2.Id,
+                Mode = "Restaurant",
+                Day = "Holiday"
+            });
+            
+            // Add opening hours for Copenhagen Bistro - Takeaway mode
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Takeaway",
+                Day = "Monday",
+                StartTime = new TimeSpan(9, 0, 0),
+                EndTime = new TimeSpan(19, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Takeaway",
+                Day = "Tuesday",
+                StartTime = new TimeSpan(9, 0, 0),
+                EndTime = new TimeSpan(19, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Takeaway",
+                Day = "Wednesday",
+                StartTime = new TimeSpan(9, 0, 0),
+                EndTime = new TimeSpan(19, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Takeaway",
+                Day = "Thursday",
+                StartTime = new TimeSpan(9, 0, 0),
+                EndTime = new TimeSpan(19, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Takeaway",
+                Day = "Friday",
+                StartTime = new TimeSpan(9, 0, 0),
+                EndTime = new TimeSpan(20, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Takeaway",
+                Day = "Saturday",
+                StartTime = new TimeSpan(10, 0, 0),
+                EndTime = new TimeSpan(20, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Takeaway",
+                Day = "Sunday",
+                StartTime = new TimeSpan(11, 0, 0),
+                EndTime = new TimeSpan(17, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Takeaway",
+                Day = "Holiday"
+            });
+            
+            // Add opening hours for Copenhagen Bistro - Buffet mode
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Buffet",
+                Day = "Monday",
+                StartTime = new TimeSpan(12, 0, 0),
+                EndTime = new TimeSpan(15, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Buffet",
+                Day = "Tuesday",
+                StartTime = new TimeSpan(12, 0, 0),
+                EndTime = new TimeSpan(15, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Buffet",
+                Day = "Wednesday",
+                StartTime = new TimeSpan(12, 0, 0),
+                EndTime = new TimeSpan(15, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Buffet",
+                Day = "Thursday",
+                StartTime = new TimeSpan(12, 0, 0),
+                EndTime = new TimeSpan(15, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Buffet",
+                Day = "Friday",
+                StartTime = new TimeSpan(12, 0, 0),
+                EndTime = new TimeSpan(15, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Buffet",
+                Day = "Saturday",
+                StartTime = new TimeSpan(12, 30, 0),
+                EndTime = new TimeSpan(15, 30, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Buffet",
+                Day = "Sunday",
+                StartTime = new TimeSpan(13, 0, 0),
+                EndTime = new TimeSpan(16, 0, 0)
+            });
+            
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Buffet",
+                Day = "Holiday"
+            });
+            
+            // Add opening hours for Copenhagen Bistro - Special Events mode
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Special Events",
+                Day = "Monday",
+                StartTime = new TimeSpan(17, 0, 0),
+                EndTime = new TimeSpan(22, 0, 0)
+            });
+
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Special Events",
+                Day = "Tuesday",
+                StartTime = new TimeSpan(17, 0, 0),
+                EndTime = new TimeSpan(22, 0, 0)
+            });
+
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Special Events",
+                Day = "Wednesday",
+                StartTime = new TimeSpan(17, 0, 0),
+                EndTime = new TimeSpan(22, 0, 0)
+            });
+
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Special Events",
+                Day = "Thursday",
+                StartTime = new TimeSpan(17, 0, 0),
+                EndTime = new TimeSpan(22, 0, 0)
+            });
+
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Special Events",
+                Day = "Friday",
+                StartTime = new TimeSpan(17, 0, 0),
+                EndTime = new TimeSpan(23, 0, 0)
+            });
+
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Special Events",
+                Day = "Saturday",
+                StartTime = new TimeSpan(17, 0, 0),
+                EndTime = new TimeSpan(23, 0, 0)
+            });
+
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Special Events",
+                Day = "Sunday",
+                StartTime = new TimeSpan(17, 0, 0),
+                EndTime = new TimeSpan(21, 0, 0)
+            });
+
+            context.OpeningHours.Add(new OpeningHours
+            {
+                ResturantId = restaurant2.Id,
+                Mode = "Special Events",
+                Day = "Holiday"
+            });
+            
+            // Add gallery images for Copenhagen Bistro
+            context.GalleryImages.Add(new GalleryImage
+            {
+                ResturantId = restaurant2.Id,
+                ImageUrl = "/images/gallery/one.jpg"
+            });
+
+            context.GalleryImages.Add(new GalleryImage
+            {
+                ResturantId = restaurant2.Id,
+                ImageUrl = "/images/gallery/two.jpg"
+            });
+
+            context.GalleryImages.Add(new GalleryImage
+            {
+                ResturantId = restaurant2.Id,
+                ImageUrl = "/images/gallery/three.jpg"            });
+            
             await context.SaveChangesAsync();
         }
     }
